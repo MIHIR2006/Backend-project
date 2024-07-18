9b51872fcf4e21aeecfb23280f7d84d51dd33017
MIHIR2006 <mihirgoswami2006@gmail.com>
2024-07-18 11:30:59 +0530

    fix register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js"
 import { User } from "../models/User.model.js";
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
-import { ApiResponse } from "../utils/ApiResponce.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 
 
@@ -30,7 +30,7 @@ console.log("email: ", email);
         throw new ApiError(400,"All fields are required")
     }
 
-    const existedUser= User.findOne({
+    const existedUser= await User.findOne({
         $or:[{username},{email}]
     })
 
@@ -38,8 +38,15 @@ console.log("email: ", email);
         throw new ApiError(409,"Username or Email is already exist ")
     }
 
+    // console.log(req.files);
+
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path; 
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path; 
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400,"Avatar file is required")
@@ -58,19 +65,19 @@ console.log("email: ", email);
         coverImage: coverImage?.url || "",
         email,
         password,
-        username: username.toLowercase()
+        username: username.toLowerCase()
     })
 
     const createdUser = await User.findById(user._id).select(
         "-password -refreshToken"
     )
 
-    if (!CreatedUser) {
+    if (!createdUser) {
         throw new ApiError(500,"Something went wrong while registering the user ")
     }
 
     return res.status(201).json(
-        new ApiResponse(200, CreatedUser,"User registered successfully")
+        new ApiResponse(200, createdUser,"User registered successfully")
     )
 } )
 
