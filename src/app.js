ef3e262114fd6343b74bdc3a4936c21d0c027ece
MIHIR2006 <mihirgoswami2006@gmail.com>
2024-07-31 16:42:25 +0530

    BUGS FIX

--- src/app.js
+++ src/app.js
@@ -22,6 +22,6 @@ import userRouter from './routes/user.routes.js'
 
 app.use("/api/v1/users",userRouter)
 
-//https:localhost:3000/api/v1/users/register
+//https:localhost:8000/api/v1/users/register
 
 export {app}
